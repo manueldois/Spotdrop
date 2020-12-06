9c02a63c8b85a31e5333280d520da2e9cedfd4fa
Manuel <manuelsilvadois@gmail.com>
2020-12-06 21:30:33 +0000

    Local background photos

--- public/src/js/home_page.js
+++ public/src/js/home_page.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             bck_img_num = img_holders.length-1;
             img_holders.removeClass("transparent")
         }
-    },2000);
+    },3000);
   
     $('#btn-goto-tutorial').click(function (){
         $('html, body').animate({
